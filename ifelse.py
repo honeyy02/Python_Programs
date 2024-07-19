eac3da4a9d628ba4fa5b0819530cfd4a3959c351
Honey Sharma <honeysharma0224@gmail.com>
2024-07-19 13:18:26 +0530

    basic program of python

---
+++ ifelse.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter the num1 "))
+num2 = int(input("Enter the num2 "))
+if num1>num2:
+    print("num1 is greater than num2")
+elif num1==num2:
+    print("numbers are equal")
+else:
+    print("num2 is greater than num1")
---
+++ loops.py
@@ -0,0 +1,9 @@
+#factorial of number
+fact = 1
+i=1
+num1 = int(input("Enter the number: "))
+while i<=num1:
+    fact=fact*i
+    i+=1
+
+print(fact)
